6a75ea2a2bd6cc5d096ab62e7f1d7284922eaf1f
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-16 13:10:32 -0800

    creates a new c file that prints a line of string when executed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/*
+ * Main - Entry point
+ *
+ * Return: Always 0 ( Successful )
+ */
+
 int main(void)
 {
-	char MyStr[]="Programming is like building a multilingual puzzle";
-	puts(MyStr);
-	return 0;
+	char MyStr[] = "\"Programming is like building a multilingual puzzle";
+	puts (MyStr);
+	return (0);
 }
 
