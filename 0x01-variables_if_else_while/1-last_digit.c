a9ef7f43453fb0bb5f650d8f388adb977b4cb67c
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-17 15:22:12 -0800

    checks the last digit of n and prints it under some conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@
 int main(void)
 {
 	int n;
+	int i;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int i;
 
 	i = 0;
 	while (i < n)
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * 
+ * main - iterates on each letter of the alphabet and prints it using putchar and while loop
+ *
+ * Return: Always 0 ( Successful )
+ */
+int main(void)
+{
+
