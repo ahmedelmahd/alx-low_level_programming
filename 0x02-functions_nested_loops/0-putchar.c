a3a822b8ab019237556f6662e673b8c3045c03ea
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-21 09:18:29 -0800

    first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <unistd.h>
+/**
+ * adds the ability to use the function strlen()
+ *
+ * adds the ability to use the function write()
+ *
+ * a program that prints to the stdout without printf
+ */
+int main(void)
+{
+	char *c = "_putchar\n";
+	int file = 1; // printing to the variable file which is standard output
+	write (file, c, strlen(c));
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 #!/bin/bash
 created a header file that has the functions i'll be using in this project
+prints "_putchar" to the standard output
