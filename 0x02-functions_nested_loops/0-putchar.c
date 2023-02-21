c7f0214e6d9e965fc0cb14eb1436808a51e4b42a
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-21 09:22:54 -0800

    prints putchar to stdout

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,15 @@
 #include <string.h>
 #include <unistd.h>
 /**
- * adds the ability to use the function strlen()
+ * main - Entry point
  *
- * adds the ability to use the function write()
- *
- * a program that prints to the stdout without printf
+ * Return: Always 0 ( Successful )
  */
 int main(void)
 {
 	char *c = "_putchar\n";
 	int file = 1; /* printing to the variable file which is standard output */
-	write (file, c, strlen(c));
+	
+	write(file, c, strlen(c));
 	return (0);
 }
