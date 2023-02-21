12ce65693eb186bb4f3d6c1b5821bffa17b42802
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-21 09:37:15 -0800

    prints alphabet in lowercase then adds a newline

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - entry point of program
+  *
+  * print_alphabet: Calls the function defined in main.h
+  *
+  * returns an int 0
+  */
+
+int main(void)
+{
+        print_alphabet();
+        return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 #!/bin/bash
 created a header file that has the functions i'll be using in this project
 prints "_putchar" to the standard output
+calls a function protoype that prints all the alphabet, in lowercase, and adds a newline
