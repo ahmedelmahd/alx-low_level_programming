2b0227bd74e16e4877d26dc9b51b81e0d6d30d64
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-21 14:55:03 -0800

    writes _putchar to stdout

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,23 @@
 #include "main.h"
+
 /**
  * main - Entry point
- *
  * Return: Always 0 ( Successful )
  */
-int main(void)
-{
-        int string[] = {95, 112, 117, 116, 99, 104, 97, 114};
 
-        for (int i = 0; i < sizeof(string); i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
-        return (0);
+int main(void)
+
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
 }
