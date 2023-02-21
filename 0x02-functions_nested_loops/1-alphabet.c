6b98a900f8bc9ceceac7f7070e26b5c4895533cb
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-21 09:54:46 -0800

    prints alphabet from a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - entry point of program
+  *
+  * print_alphabet: Calls the function defined in main.h
+  *
+  * returns an int 0
+  *
+  * Return: Always 0 ( Successful )
+  */
+int main(void)
+{
+	print_alphabets();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 ( Successful )
  */
-void print_alphabets(void)
+void print_alphabet(void)
 {
 	char c = 'a';
 	int file = 1;
