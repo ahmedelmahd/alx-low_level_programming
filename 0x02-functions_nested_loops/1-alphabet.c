18720f5d2250b2ca533353ee82b8f08aba258f06
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-21 09:38:43 -0800

    prints alphabet in lowercase then adds a newline

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,11 @@
   * print_alphabet: Calls the function defined in main.h
   *
   * returns an int 0
+  *
+  * Return: Always 0 ( Successful )
   */
-
 int main(void)
 {
-        print_alphabet();
-        return (0);
+	print_alphabet();
+	return (0);
 }
