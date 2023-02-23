166723f88b96ae47f45fe1eadcba9f28c39a334d
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-23 15:21:17 -0800

    prints a line (n) number of times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints a line (n) times
+ * @n: number of times the line should be printed
+ * Doesn't require a return value
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -5,3 +5,4 @@ multiplies 2 ints
 prints digits from 0 to 9, followed by a new line
 prints numbers from 0 to 9 except 2 and 4
 prints from 0 to 14 10 timesd
+prints '_' n number of times
