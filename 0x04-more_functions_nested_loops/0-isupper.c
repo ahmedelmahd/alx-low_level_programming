80065670d0a85b511d2eb4db60f12132bdba8ea9
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-23 14:16:31 -0800

    checks if a char is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main() 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 ( Successful )
+ */
+int main(void)
 {
-    char char_input;
+	char char_input;
 
-    printf("Enter a character: ");
-    scanf("%c", &char_input);
+	printf("Enter a character: ");
+	scanf("%c", &char_input);
 
-    if (isupper(char_input)) 
-    {
-        printf("%c:", char_input);
-	printf("1\n");
-    }
-    else 
-    {
-        printf("%c:", char_input);
-    	printf("0\n");
-    }
-    return 0;
+	if (isupper(char_input))
+	{
+		printf("%c:", char_input);
+		printf("1\n");
+	}
+	else
+	{
+		printf("%c:", char_input);
+		printf("0\n");
+	}
+	return (0);
 }
 
