dc4b41ca964067d7928edae731f19ec9748343a8
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-27 13:15:35 -0800

    returns the length of the string s

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,9 +4,10 @@
  * int_strlen - returns the length of a string
  * @s: the string of characters
  *
- * Return: void
+ * Return: Always 0 ( Successful )
  */
 int _strlen(char *s)
 {
 	_putchar(sizeof(*s) - 1);
+	return (0);
 }
