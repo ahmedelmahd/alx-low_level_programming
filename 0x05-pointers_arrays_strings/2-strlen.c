7c4f97a3232c2f17c05915929cf47feb27105e99
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-27 13:12:33 -0800

    returns the length of the string s

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int_strlen - returns the length of a string
+ * @s: the string of characters
+ *
+ * Return: void
+ */
+void int_strlen(char *s)
+{
+	_putchar(sizeof(*s) - 1);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 #!/bin/bash
 changes the value of the pointer n to 98
 swaps the values of 2 integers
+returns the length of the string 's'
