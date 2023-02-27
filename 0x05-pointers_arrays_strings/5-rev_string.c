3160da08d45eb3bd43b52b68e24acbd76e45c9b5
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-02-27 15:24:01 -0800

    prints a reversed s array of chars

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string to reverse
+ * return: void
+ */
+void rev_string(char *s)
+{
+	int j = 0;
+	int i = 0;
+
+	while (s[i] != '\n')
+	{
+		i++
+	}
+	while (s[j] != i - 1)
+	{
+		_putchar(s[j]);
+		j--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -4,3 +4,4 @@ swaps the values of 2 integers
 returns the length of the string 's'
 prints a string to stdout
 prints a reversed string to stdout
+prints a reversed string
