0e6e00db4e7d023f2855cd082d0ecc647969d509
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-06 13:12:04 -0800

    prints value of pointer s into b

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * _memset(char*s, char b, unsigned int n) - fills memory with a constant byte
+ * *_memset - fills memory with a constant byte
  *
  * @s: pointer to memory n
  * @b: bytes to be entered into n
  * @n: number of the first bytes in memory
- * return: a pointer to memory area s
+ * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (b != '\0')
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		*s = b;
+		s[i] = b; /* or *(s + i) for pointer arithmetic */
 	}
 	return (s);
 }
