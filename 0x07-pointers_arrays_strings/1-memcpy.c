445a396b754781291f25e5a1b88217b02dab8649
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-06 13:35:08 -0800

    copies n bytes from src to dest

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_memcpy - copies n bytes from src to dest memory areas
+ * @dest: the place to paste copied bytes to
+ * @src: the place to copy bytes from
+ * @n: number of bytes to copy
+ *
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			dest[j] = src[i];
+		}
+	}
+	return (dest);
+}
