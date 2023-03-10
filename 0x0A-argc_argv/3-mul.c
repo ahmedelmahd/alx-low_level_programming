8b634089ad34f601601b003f6f11b878204adbd0
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-10 08:02:35 -0800

    a program to multiply from the command line argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - entry point for the program
+ * main - the main entry point for the program
  * @argc: command line arguments counter
- * @argv: pointer array of pointers that point to command-line arguments
- * passed by the user
- * Return: product of arguments
+ * @argv: a pointer that points to an array of pointers
+ * which point to command line arguments
+ * Return: an integer
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	
+	int i;
+	int mul = 1;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
 }
