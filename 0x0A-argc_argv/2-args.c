0c0b486b22ff44ef06179e507300a25d570c6f45
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-10 07:43:51 -0800

    prints every argument it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
  * main - the entry point for the program
  * @argc: the command line-arguments counter
  * @argv: a pointer to an array of pointers which point
- * to a CL argument passed by the user
+ * to an argument passed by the user
  * Return: an integer
  */
 int main(int argc, char **argv)
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point for the program
+ * @argc: command line arguments counter
+ * @argv: pointer array of pointers that point to command-line arguments
+ * passed by the user
+ * Return: product of arguments
+ */
+int main(int argc, char **argv)
+{
+	
+}
