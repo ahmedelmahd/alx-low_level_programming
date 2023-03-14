3580b3a6dc07f973ed723f91dedc6066c5e3e84a
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-14 09:22:46 -0700

    creates an duplicated array that prints exactly what the string parameter has

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer that contains a copy of a newly allocated memory address
+ * that has a string as a paramater
+ * @str: the string provided as a parameter
+ *
+ * Return: a pointer on success, NULL if otherwise
+ */
+char *_strdup(char *str)
+{
+	int length;
+	char *cp_str;
+	int i;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	cp_str = malloc(sizeof(char) * length + 1);
+	if (cp_str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= length; i++)
+	{
+		cp_str[i] = str[i];
+	}
+	return (cp_str);
+}
