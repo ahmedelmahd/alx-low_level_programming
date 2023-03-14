e3448f5b4da5e188b7c13bdb079b5f0df2319559
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-14 10:07:50 -0700

    a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,8 +16,13 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = " ";
 	}
-	if (s2 == NULL)
+	else if (s2 == NULL)
 	{
+		s2 = " ";
+	}
+	else if (s1 == NULL && s2 == NULL)
+	{
+		s1 = " ";
 		s2 = " ";
 	}
 	len1 = 0;
