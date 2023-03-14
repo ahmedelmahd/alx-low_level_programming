d8ab03c20c122197e59c51fec1811c6298af5542
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-14 15:01:58 -0700

    a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,57 +1,38 @@
-#include "main.h"
-#include <stddef.h>
 #include <stdlib.h>
+#include "main.h"
 /**
- * str_concat -  a function that concatenates two strings
- * @s1: 1st string
- * @s2: 2nd string
- * Return: returns a pointer to the allocated memory
+ * str_concat - concatenates two strings together
+ * @s1: first string
+ * @s2: second string
+ * Return: a pointer to the concatenated string
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1, len2;
-	char *cat_str;
+	int len1, len2, i;
+	char *res;
 
 	if (s1 == NULL)
-	{
-		s1 = " ";
-	}
-	else if (s2 == NULL)
-	{
-		s2 = " ";
-	}
-	else if (s1 == NULL && s2 == NULL)
-	{
-		s1 = " ";
-		s2 = " ";
-	}
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	len1 = 0;
-	while (s1[len1] != '\0')
-	{
+	while (s1[len1])
 		len1++;
-	}
+
 	len2 = 0;
-	while (s2[len2] != '\0')
-	{
+	while (s2[len2])
 		len2++;
-	}
-	cat_str = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (cat_str != NULL)
-	{
-		i = 0;
-		while (s1[i] != '\0')
-		{
-			cat_str[i] = s1[i];
-			i++;
-		}
-		j = 0;
-		while (s2[j] != '\0')
-		{
-			cat_str[i] = s2[j];
-			i++;
-			j++;
-		}
-		cat_str[i] = '\0';
-	}
-	return (cat_str);
+
+	res = malloc(len1 + len2 + 1);
+	if (!res)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		res[i] = s1[i];
+	for (i = 0; i < len2; i++)
+		res[i + len1] = s2[i];
+
+	res[len1 + len2] = '\0';
+	return (res);
 }
