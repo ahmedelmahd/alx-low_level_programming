87dc590d37d8ed29fdbde7e1a5d1796ba2626e4f
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-20 15:25:09 -0700

    creates a function that prints the values of struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - a function that prints a struct dog
+ * @d: a pointer to the struct dog
+ * Returns: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		printf("nil");
+	}
+	else if (d->owner == NULL)
+	{
+		printf("nil");
+	}
+	else
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -2,3 +2,4 @@
 
 creates a structure with name,age,owner variables
 a function that creates a variable of type struct dog
+Creates a function that prints the values of struct dog
