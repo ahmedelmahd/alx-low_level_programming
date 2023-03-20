3fcc268d8196aadd08617689b77a74ba04e804ad
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-20 14:21:59 -0700

    a structure that has 3 elements-> basic info about a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure that has 3 elements
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of the owner of the dog
+ * Description: a structure that has 3 elements:
+ * name of the dog, age of the dog, and the name of the dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
