b5f6db7558507cfbcdeead64dd60431d195842bc
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-20 06:47:12 -0700

    main file containing all functions and macros

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -6,7 +8,7 @@ void swap_int(int *a, int *b);
 
 int _strlen(char *s);
 
-void _puts(char *str);
+void _puts(char *s);
 
 void print_rev(char *s);
 
@@ -19,3 +21,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
+#endif
--- 0x0D-preproccesor/main.h
+++
@@ -1,4 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include "object_like_macro.h"
-#endif
