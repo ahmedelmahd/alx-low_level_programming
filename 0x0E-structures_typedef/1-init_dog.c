f3d7fcaf20cb8f1ba8e5d1c5757a6f77eae39ca0
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-20 14:50:20 -0700

    a function that creates a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - a function that creates a variable of type dog
+ * struct dog - a structure that has 3 elements
+ * @d: the name of the structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ * Description: a struct that has 3 elements ==
+ * ==> a dog's name, a dog's age, and the name of the dog owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
