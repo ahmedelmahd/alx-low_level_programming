8333ebe0936b6c81b33b75ccf52517521c2fd1a4
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-22 12:02:40 -0700

    debugging

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stddef.h>
 /**
  * print_name -  a function that prints a name
  * @name: the name to be printed
@@ -8,5 +8,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+	{
+		return;
+	}
 	f(name);
 }
