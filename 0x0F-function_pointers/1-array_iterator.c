257e3742b24e4dc96b46925371bbfa639afb785d
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-22 12:48:07 -0700

    a function that executes itself on every element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	i = 0;
+	if (action == NULL || array == NULL)
+	{
+		return;
+	}
 	while (i < size)
 	{
 		action(array[i]);
