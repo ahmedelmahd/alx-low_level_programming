647ff354f4548d6ee29a8e4e2b7636f5d95b4bfc
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-03-23 15:40:26 -0700

    a function that prints integers passed to it as parameters

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
-	int arg;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		arg = va_arg(args, int);
+		printf("%d", va_arg(args, int));
 		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
-		printf("%d", arg);
 	}
 	va_end(args);
 	printf("\n");
