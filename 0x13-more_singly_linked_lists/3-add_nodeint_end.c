24a9e7f4eaa165679645bb3c57fdf674222746cd
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-04-03 15:07:11 -0700

    a function that adds a new node to the end of the linked list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_nodeint_end - a function that adds a new node to the end of the list
+ * @head: a pointer to a pointer to the first node
+ * @n: the new node's value
+ * Return: the address of the new node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	listint_t *last_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head  == NULL)
+	{
+		new_node = *head;
+	}
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+		last_node->next = new_node;
+	}
+	return (new_node);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -3,3 +3,5 @@ a function that prints all the elements of a listint_t list.
 
 a function that prints the number of elements in a single linked list
 a function that adds a new node to the start of the list
+
+a function that adds a new node to the end of the linked list
