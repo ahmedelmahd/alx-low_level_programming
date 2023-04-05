89ce6d7a5d0e997b8e7d56e9c8059eed5e17ab60
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-04-04 17:03:26 -0700

    a function that returns the nth node of the linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -22,7 +22,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		current_node = current_node->next;
 		i++;
 	}
-	if (i == index && current_node != NULL)
+	if ((i == index) && (current_node != NULL))
 	{
 		return (current_node);
 	}
