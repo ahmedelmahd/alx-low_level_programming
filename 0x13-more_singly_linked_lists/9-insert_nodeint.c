eb8f821defd6e301811f54e95674465e2ce417ba
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-04-04 19:10:13 -0700

    a function that adds a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *current_node;
-        listint_t *new_node;
+	listint_t *new_node;
 
 	if (head == NULL)
 	{
