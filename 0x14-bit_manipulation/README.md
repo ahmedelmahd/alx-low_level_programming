21a06e10018a51720756d53edb9d6ac950564cf4
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-04-08 16:02:41 -0700

    a function that returns the bit value at given index of n

--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -3,3 +3,5 @@
 a function that converts a binary number to an unsigned int.
 
 a function that prints the binary representation of a number.
+
+a function that returns the value of a bit at a given index.
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
