4f4cd14e10de5f9ad14fdaaed0f052a5d7f09084
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-05-08 15:44:07 -0700

    file and input/output tasks

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ * @filename: the name of the file
+ * @text_content: the content of the file
+ * Return: 1 if successful, -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int new_file, fd, count;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (text_content != NULL)
+	{
+		count = 0;
+		while (text_content[count])
+		{
+			count++;
+		}
+		new_file = write(fd, text_content, count);
+		if (new_file == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+a function that reads a text file and prints it to the POSIX standard output.
+
+a function that creates a file
+
+a function that appends a text to an already created file
+
+a function that copies text from one file to another
