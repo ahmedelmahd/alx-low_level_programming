426b9c8fb25e524a5809185d1171d287a6dc7a7b
ahmed elmahd <ahmedsamir.0120495@gmail.com>
2023-07-06 12:00:59 -0700

    3rd, 4th and 5th tasks

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - a function that returns the number of flips needed to flip n > m
+ * @n: the first number
+ * @m: the second number
+ * Return: the number of flips needed to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int current, xclusive;
+	int flips, length;
+
+	flips = 0;
+	xclusive = n ^ m;
+	for (length = 63; length >= 0; length--)
+	{
+		current = xclusive >> length;
+		if (current & 1)
+		{
+			flips++;
+		}
+	}
+	return (flips);
+}
