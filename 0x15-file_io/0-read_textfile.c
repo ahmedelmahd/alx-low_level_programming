b52678ff50e572e6422042f327e0e26712b149d8
ahmedelmahd <ahmedsamir.0120495@gmail.com>
2023-08-07 15:28:43 -0700

    all programs and tests with main file and readme

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and prints it to stdout
+ * @filename: the file that we will read from
+ * @letters: the number of letters are in the file
+ * Return: the actual number of letters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t actual_letters, written;
+	ssize_t file_pointer;
+	char *buffer;
+
+	file_pointer = open(filename, O_RDONLY);
+	if (file_pointer == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	actual_letters = read(file_pointer, buffer, letters);
+	written = write(stdout, buffer, actual_letters);
+
+	free(buffer);
+	close(file_pointer);
+	return (written);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - a function that adds a new text into a file
+ * @filename: the file that we will add to
+ * @text_content: the text that we will add to the filename
+ * Return: 1 if successful, -1 otherwise
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int new_file, fd, count;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_APPEND | O_WRONLY);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		count = 0;
+		while (text_content[count] != '\0')
+		{
+			count++;
+		}
+		new_file = write(fd, text_content, count);
+		if (newfile == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
